dbee95f62828b8e5dd0e7c28faf1cdf91dd13890
Debora Carvalho <dbndy3@gmail.com>
2024-09-06 13:40:23 -0300

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
     <>
       <InputPesquisa />
       <div className="mx-auto max-w-screen-xl">
-        <h1 className="mt-4 mb-4 text-w-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-6xl dark:text-white">BlockBuster Matrix <span className="text-red-600 dark:text-red-500">the world's #1</span> rental house game.</h1>
+        <h1 className="mt-4 mb-4 text-w-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-6xl dark:text-white">BlockBuster Matrix <span className="text-red-600 dark:text-red-500">the worlds #1</span> rental house game.</h1>
         <p className="mt-2 mb-4 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
 
         {/* GRID GAMES */}
