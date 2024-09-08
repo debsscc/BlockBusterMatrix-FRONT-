af9e69881e3905a4fb69988fa7dd2528298e5d94
Debora Carvalho <dbndy3@gmail.com>
2024-09-08 18:39:54 -0300

    aa

--- src/components/Headers.tsx
+++ src/components/Headers.tsx
@@ -5,7 +5,7 @@ export function Header() {
         <nav className="bg-white dark:bg-gray-900 w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600 sticky">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="https://www.elocalx.com/image/cache/manufacturers/team-matrix-elocalx-600x315.png" className="h-8"/>
+                    <img src="/public/logo.png" className="h-8" alt="Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Blockbuster Matrix</span>
                 </Link>
                 <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
@@ -32,6 +32,8 @@ export function Header() {
                             <Link href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</Link>
                         </li>
                     </ul>
+                    <div>
+                    </div>
                 </div>
             </div>
         </nav>
