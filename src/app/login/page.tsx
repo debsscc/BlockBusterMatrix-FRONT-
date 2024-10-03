2e22ca1b8339ffc9afa7307f5a4e83c6c1221790
Debora Carvalho <dbndy3@gmail.com>
2024-10-03 10:42:17 -0300

    Login White Label

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -83,7 +83,8 @@ export default function Login() {
                                 <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500
                                 text-red-500">Forgot password?</a>
                             </div>
-                            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                            <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-900 dark:hover:bg-blue-900 dark:focus:ring-blue-800">Sign in</button>
+                            
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                                 Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500
                                 text-red-500">Sign up</a>
